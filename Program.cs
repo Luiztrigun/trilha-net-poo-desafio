31631181a76a9185dbcc96acbd2c846c393db155
Luiztrigun <luizfelipedealmeida@gmail.com>
2022-10-05 14:10:44 -0300

    Desafio Celular Poo alterado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "2345678", modelo: "Modelo55" ,imei: "5698MMPKL" , memoria: 128 );
+nokia.Ligar();
+nokia.InstalarAplicativo("Zapzap");
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone Iphone = new Iphone(numero: "2345678", modelo: "Modelo55" ,imei: "5698MMPKL" , memoria: 128 );
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Telegram");
+
+
+
+
+
